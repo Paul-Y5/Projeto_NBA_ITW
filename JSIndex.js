84d341e4370be89e288ed9d0221dd2d6f10271a5
Paul-Y5 <pauladascardoso@gmail.com>
2023-12-02 22:35:01 +0000

    Dark.mode

--- JSIndex.js
+++ JSIndex.js
@@ -1,6 +1,7 @@
 /* Dark-Mode */
 document.getElementById("dark-mode").addEventListener("click", () => {
   if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
+    document.documentElement.setAttribute("data-bs-theme", "light");
   } else {
     document.documentElement.setAttribute("data-bs-theme", "dark");
   }
