896fa165e13d88e290c70b42bb7d72d461c10167
TSH-19 <tomas.santos.hilario@gmail.com>
2023-12-16 01:36:22 +0000

    ...

--- states.js
+++ states.js
@@ -37,6 +37,29 @@ var vm = function () {
     for (var i = 1; i <= size; i++) list.push(i + step);
     return list;
   };
+ 
+  self.toggleFavourite = function (id) {
+    if (self.favourites.indexOf(id) == -1) {
+        self.favourites.push(id);
+    }
+    else {
+        self.favourites.remove(id);
+    }
+    localStorage.setItem("fav", JSON.stringify(self.favourites()));
+};
+self.SetFavourites = function () {
+    let storage;
+    try {
+        storage = JSON.parse(localStorage.getItem("fav"));
+    }
+    catch (e) {
+        ;
+    }
+    if (Array.isArray(storage)) {
+        self.favourites(storage);
+    }
+}
+self.favourites = ko.observableArray([])
 
   //--- Page Events
   self.activate = function (id) {
@@ -53,7 +76,7 @@ var vm = function () {
       self.hasPrevious(data.HasPrevious);
       self.pagesize(data.PageSize);
       self.records(data.Records);
-      //self.SetFavourites();
+      self.SetFavourites();
     });
   };
 
--- teams.js
+++ teams.js
@@ -54,9 +54,31 @@ var vm = function () {
         self.pagesize(data.PageSize);
         self.totalPages(data.TotalPages);
         self.totalRecords(data.TotalRecords);
-        //self.SetFavourites();
+        self.SetFavourites();
       });
     };
+    self.toggleFavourite = function (id) {
+      if (self.favourites.indexOf(id) == -1) {
+          self.favourites.push(id);
+      }
+      else {
+          self.favourites.remove(id);
+      }
+      localStorage.setItem("fav", JSON.stringify(self.favourites()));
+  };
+  self.SetFavourites = function () {
+      let storage;
+      try {
+          storage = JSON.parse(localStorage.getItem("fav"));
+      }
+      catch (e) {
+          ;
+      }
+      if (Array.isArray(storage)) {
+          self.favourites(storage);
+      }
+  }
+  self.favourites = ko.observableArray([])
   
     //--- Internal functions
     function ajaxHelper(uri, method, data) {
