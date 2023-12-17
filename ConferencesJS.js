0ea2b9b667fdc7cc90b849058f75a581fec0c485
Paul-Y5 <pauladascardoso@gmail.com>
2023-12-17 21:48:38 +0000

    styles

--- ConferencesJS.js
+++ ConferencesJS.js
@@ -38,24 +38,6 @@ var vm = function () {
     for (var i = 1; i <= size; i++) list.push(i + step);
     return list;
   };
-  self.toggleFavourite = function (id) {
-  if (self.favourites.indexOf(id) == -1) {
-    self.favourites.push(id);
-  } else {
-    self.favourites.remove(id);
-  }
-  localStorage.setItem("fav", JSON.stringify(self.favourites()));
-};
-self.SetFavourites = function () {
-  let storage;
-  try {
-    storage = JSON.parse(localStorage.getItem("fav"));
-  } catch (e) {}
-  if (Array.isArray(storage)) {
-    self.favourites(storage);
-  }
-};
-self.favourites = ko.observableArray([]);
 
   //--- Page Events
   self.activate = function (id) {
@@ -73,7 +55,6 @@ self.favourites = ko.observableArray([]);
       self.hasNext(data.HasNext);
       self.hasPrevious(data.hasPrevious);
       self.records(data.Records);
-      self.SetFavourites();
     });
   };
 
